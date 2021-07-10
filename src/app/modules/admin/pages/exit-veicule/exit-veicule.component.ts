b34db56ff9b3ab18f84a77e7cddd4575fbb9152f
DiguyaDeveloper <diegocecconcwb@outlook.com>
2021-07-10 12:02:38 -0300

    feature/Modules initial - Criado modulos iniciais architeture e tela de registro de carro

---
+++ src/app/modules/admin/pages/exit-veicule/exit-veicule.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exit-veicule',
+  templateUrl: './exit-veicule.component.html',
+  styleUrls: ['./exit-veicule.component.scss']
+})
+export class ExitVeiculeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
